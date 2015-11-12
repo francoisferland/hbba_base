bfcf8783b9b78f5b26cd7dbff3972069de3aa7c5
François Ferland <git@francoisferland.com>
2015-11-12 15:43:01 +0100

    Fix to setup.py concerning scripts.

--- iw_tools/setup.py
+++ iw_tools/setup.py
@@ -5,11 +5,10 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
         ##  don't do this unless you want a globally visible script
-        scripts=['scripts/iw',
-                 'scripts/iw_console',
+        scripts=['scripts/iw_console',
                  'scripts/iw_observer',
                  'scripts/get_intention'], 
-        packages=['iw'],
+        packages=['iw_tools'],
         package_dir={'': 'src'}
         )
 
