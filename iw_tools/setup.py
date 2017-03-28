23f522cea294029fadc24aeed2abbbf788bb43d6
François Ferland <git@francoisferland.com>
2017-03-28 15:46:17 +0000

    Adding missing scripts in install clauses of iw_tools.

--- iw_tools/setup.py
+++ iw_tools/setup.py
@@ -6,7 +6,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
         ##  don't do this unless you want a globally visible script
         scripts=['scripts/iw_console',
-                 'scripts/get_intention'], 
+                 'scripts/get_intention',
+                 'scripts/load_desires',
+                 'scripts/events_to_string.py'], 
         packages=['iw_tools'],
         package_dir={'': 'src'}
         )
